2dfad4d3a741b712c46ae3b03e030371ed1f4118
afonsusousa <afonso.rocha@outlook.com>
2025-04-24 17:17:23 +0100

    header norm

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,25 +1,38 @@
-#ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amagno-r <amagno-r@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/20 17:57:37 by amagno-r          #+#    #+#             */
+/*   Updated: 2025/04/20 20:34:10 by amagno-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#ifndef BUFFER_SIZE 
-#define BUFFER_SIZE 42
-#endif
-typedef struct	s_list
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+typedef struct s_list
 {
 	int				fd;
 	char			*str;
 	struct s_list	*next;
 }	t_list;
 
-char    *ft_strcnpy(t_list *lst, char *dest, int fd);
-int     ft_newlen(char *str);
-t_list  *ft_lstlast(t_list *lst, int fd);
-void    ft_add_back(t_list **lst, char *str, int fd);
-void    ft_freelist(t_list **lst, t_list *to_replace, int fd);
-int     ft_foundnew(t_list *lst, int fd);
-void    get_list(t_list **dest, int fd);
-char    *serve_line(t_list *lst, int fd);
-void    clean_list(t_list **lst, int fd);
-char    *get_next_line(int fd);
+char	*ft_strcnpy(t_list *lst, char *dest, int fd);
+int		ft_newlen(char *str);
+t_list	*ft_lstlast(t_list *lst, int fd);
+void	ft_add_back(t_list **lst, char *str, int fd);
+void	ft_freelist(t_list **lst, t_list *preserve, int fd);
+int		ft_foundnew(t_list *lst, int fd);
+void	get_list(t_list **dest, int fd);
+char	*serve_line(t_list *lst, int fd);
+void	clean_list(t_list **lst, int fd);
+char	*get_next_line(int fd);
 
 #endif
